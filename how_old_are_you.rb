ece106bf97f066bd10971a061459a509d5226465
bold-sandbox-9841 <rogerdunn@gmail.com>
2018-09-27 02:50:37 +0000

    automatically backed up by learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -2,8 +2,8 @@ require_relative
 './current_age_for_birth_year.rb'
 
 puts "What year were you born?"
-birth_year = gets.to_i 
+inputyear = gets.to_i 
 
-users_age= current_age_for_birth_year(birth_year)
+users_age= current_age_for_birth_year(inputyear)
 
 puts "you are: " + users_age.to_s + " years old."
