78ed0dea67ee530f976f155acb7d937ddf16cfca
bold-sandbox-9841 <rogerdunn@gmail.com>
2018-09-27 02:52:39 +0000

    Done.

---
+++ how_old_are_you.rb
@@ -0,0 +1,9 @@
+require_relativela
+'./current_age_for_birth_year.rb'
+
+puts "What year were you born?"
+inputyear = gets.to_i 
+
+users_age= current_age_for_birth_year(inputyear)
+
+puts "you are: " + users_age.to_s + " years old."
